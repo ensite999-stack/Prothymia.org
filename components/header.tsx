317e84d2be56c408eebd7616873f97bd22382025
ensite999-stack <ensite999@gmail.com>
2026-09-15 11:34:41 +0800

    style: simplify masthead alignment

--- components/header.tsx
+++ components/header.tsx
@@ -108,12 +108,6 @@ export function Header() {
 
   return (
     <header className="site-header" data-scrolled={scrolled} data-hidden={hidden}>
-      <div className="header-left">
-        <div className="header-menu-slot"><SiteMenu /></div>
-        <Link className="header-text-link search-link" href="/search">Search</Link>
-        <Link className="header-text-link subscribe-link" href="/newsletter">Subscribe</Link>
-      </div>
-
       <button
         className="brand-link brand-button"
         type="button"
@@ -124,6 +118,11 @@ export function Header() {
       >
         Prothymia
       </button>
+
+      <div className="header-actions">
+        <Link className="header-text-link search-link" href="/search">Search</Link>
+        <div className="header-menu-slot"><SiteMenu /></div>
+      </div>
     </header>
   );
 }
